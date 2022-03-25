33bbb763a8e4a371d04bc47c260ea18d99199bb0
ajay-prajapati07 <ajay.prajapati@sourcefuse.com>
2022-03-25 08:19:25 +0530

    1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
+const path = require("path");
 const res = require("express/lib/response");
 const app = express();
 
+// setup static and middleware
+app.use(express.static("./public"));
 app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource not found</h1>");
+  res.status(404).send("Resource not found");
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000...");
+  console.log("Server is listening");
 });
